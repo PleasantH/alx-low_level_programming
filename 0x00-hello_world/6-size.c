a87740a223b867b0063453372edde432a949cbd2
Pleasant Hamilton <hamiltonpleasantn@gmail.com>
2022-10-14 15:01:42 -0700

    6sizec

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  * Return: 0 if no error, non zero value if errors
  */
 int main(void)
-{	
+{
 	printf("Size of a char: %i byte(s)\n", sizeof(char));
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
