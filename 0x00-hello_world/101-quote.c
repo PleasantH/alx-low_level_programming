0c0c5f177d7ec3f35ef5492fbe57662af061a9fa
Pleasant Hamilton <hamiltonpleasantn@gmail.com>
2022-10-16 14:19:05 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main-program entry point
- * Return: 1 on sucess 
+ * Return: 1 on sucess
  */
 int main(void)
 {
-	write(2, "and thatpiece of line is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
