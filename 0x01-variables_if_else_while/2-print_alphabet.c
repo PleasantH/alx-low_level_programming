e7fbac49322246ba2bd317680d29cbb2d0cb3a0d
Pleasant Hamilton <hamiltonpleasantn@gmail.com>
2022-10-16 16:54:21 -0700

    print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
  * main-program entry point
- * Returnn: 0 if no error, non-zero if error
+ * Return: 0 if no error, non-zero if error
  */
 int main(void)
 {
 	char i;
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		putchar(i);
