7037ce0774698e8d8f447c3d576eba6368bd9e76
Pleasant Hamilton <hamiltonpleasantn@gmail.com>
2022-10-17 15:58:05 -0700

    comb5_c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,23 +9,23 @@ int main(void)
 	int i, j;
 
 	for (i = 0; i < 99; i++)
-		{
-			for(j = i + 1; j <= 99; j++)
+	{
+		for (j = i + 1; j <= 99; j++)
 			{
-				putchar('0' + i/10);
-				putchar('0' + i%10);
+				putchar('0' + i / 10);
+				putchar('0' + i % 10);
 
 				putchar(' ');
 
-				putchar('0' + j/10);
-				putchar('0' + j%10);
+				putchar('0' + j / 10);
+				putchar('0' + j % 10);
 
 				if (i == 98 && j == 99)
 					break;
 				putchar(',');
 				putchar(' ');
 			}
-		}
+	}
 	putchar('\n');
 	return (0);
 }
