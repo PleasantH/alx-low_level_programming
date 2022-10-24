862a00fd688f269789ea8906d4d25eff84d81ad7
Pleasant Hamilton <hamiltonpleasantn@gmail.com>
2022-10-24 16:10:31 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen-Finds the length of a string
+ * @s:String pointer to the string whose length is to be found
+ * Return:returns the length of the string
+ */
+
+int_strlen(char *s)
+{
+	int p = 0;
+	/*increment up to where the last chaacter is NULL,\0*/
+	while(*(s+p) != 0)
+	{
+		p++;
+	}
+	return (p);
+}
