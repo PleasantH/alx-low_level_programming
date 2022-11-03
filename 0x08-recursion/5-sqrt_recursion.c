c58a3caa4850ecca555fd101864cec162a6a39b3
Pleasant Hamilton <hamiltonpleasantn@gmail.com>
2022-11-02 19:19:05 -0700

    sqrt.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,12 +9,12 @@ int power_operation(int n, int c)
 {
 	if (c % (n / c) == 0)
 	{
-		if (c *(n / c) == n)
+		if (c * (n / c) == n)
 			return (c);
 		else
 			return (-1);
 	}
-	return(0 + power_operation(n, c + 1));
+	return (0 + power_operation(n, c + 1));
 }
 /**
  * _sqrt_recursion - returns the natural square root of a number
